86cb70fe9e1213ca5ff72261220dd5ce125f7daf
Jason Saenz <jsigns@gmail.com>
2018-02-08 21:07:56 -0600

    DOPE

---
+++ src_dir/src1/components/HelloDiv.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HelloDiv = () => (
+  <div>
+    <h1>Hello! My name is Jason!</h1>
+    <h2>I'm super dope</h2>
+    <h3>I like:</h3>
+    <ul>
+      <li>Stuff</li>
+      <li>Things</li>
+      <li>Dogs</li>
+    </ul>
+  </div>
+);
+export default HelloDiv;
